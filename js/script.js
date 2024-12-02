63a16200a454c8aeb12b7ddaea8b6f44f3eb2574
Georges <georges@savini.org>
2024-12-02 07:45:52 +0100

    premier commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+ const hamburgerToggler = document.querySelector(".hamburger")
+ const navLinksContainer = document.querySelector(".navlinks-container");
+
+const toggleNav = () =>{
+    hamburgerToggler.classList.toggle("open")
+
+    const  ariaToggle = hamburgerToggler.getAttribute("arie-expanded")==="true" ?  "false" : "true";
+    hamburgerToggler.setAttribute("aria-expanded",ariaToggle)
+
+    navLinksContainer.classList.toggle("open")
+}
+
+hamburgerToggler.addEventListener("click",toggleNav)
+
+ new ResizeObserver(entries =>{
+     if(entries[0].contentRect.width <= 900){
+         navLinksContainer.style.transition = "transform 0.4s ease-out"
+     }
+     else{
+         navLinksContainer.style.transition= "none"
+     }
+ } ).observe(document.body)
